a46f337680bd7b106eb5b7058d32f9d0534a344b
Vital_dev <164503762+AilaVital@users.noreply.github.com>
2025-05-29 09:24:15 -0300

    Update Lista.c

--- Lista.c
+++ Lista.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+ /* float fahrenheit, celsius;
+  printf("Qual valor em graus fahrenheit voocê deseja converter para graus Celsius?\n");
+  scanf("%f", &fahrenheit);
+  celsius = (fahrenheit - 32) * 5 / 9;
+  printf("O valor em graus Celsius é: %.2f\n", celsius);
+  return 0;
+}
+*/
+  float preco;
+printf("Qual o preço do produto?\n");
+  scanf("%f", &preco);
+  if (preç)
+  printf("O preço do produto com desconto é: %.2f\n", preco * 0.1);
+  
+  
